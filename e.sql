f8ec13c11bc17e3a2003733e3a2691d4dcd54b04
neojarvis <techadmin@iamneo.ai>
2024-02-13 06:38:20 +0000

    Manual-save-commit-13/02/2024--12:08:10

---
+++ e.sql
@@ -0,0 +1,11 @@
+    -- Create the table
+    CREATE TABLE ExampleTable6 (
+        ID int,
+        Name nvarchar(255)
+    );
+
+    -- Insert values into the table
+    INSERT INTO ExampleTable6 (ID, Name) VALUES (1, 'e');
+    INSERT INTO ExampleTable6 (ID, Name) VALUES (2, 'e1');
+
+-- Select and view all data from the table
